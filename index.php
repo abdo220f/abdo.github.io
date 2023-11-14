7bbd641a95f8d4845bf8757071f6e276bcc2994e
abdo220f <150723062+abdo220f@users.noreply.github.com>
2023-11-14 14:52:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">abdo</a>
+        <nav class="navigation">
+            <a href="#services">services</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
+
+    <section class="main">
+        
+        <div>
+            <h2>welcome to<br><span>abdo website</span></h2>
+            <h3>Programmer, web developer.</h3>
+            <a href="#projects" class="main-btn">View my work</a>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                
+            </div>
+        </div>
+        
+    </section>
+
+    <section class="cards" id="services">
+        <h2 class="title">Services</h2>
+        <div class="content">
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-edit"></i>
+                </div>
+                <div class="info">
+                    <h3>Web developer</h3>
+                    <p>create a web sites using HTML CSS PHP</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="projects" id="projects">
+        <h2 class="title">Projects</h2>
+        <div class="content">
+            <div class="project-card">
+                <div class="project-image">
+                    <img src="project.PNG" />
+                    <img src="project2.PNG" />
+
+                </div>
+                <div class="project-info">
+                    <p class="project-category">Book rental</p>
+                    <strong class="project-title">
+                        <span>This site is made for renting books and We have used the language PHP HTML CSS...to create this website.</span>
+                        
+                    </strong>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+
+    <section class="cards contact" id="contact">
+        <h2 class="title">Contact</h2>
+        <div class="content">
+            
+            <div class="card">
+                <div class="icon">
+                    <i class="fas fa-envelope"></i>
+                </div>
+                <div class="info">
+                    <h3>Email</h3>
+                    <p>abdo317787@gmail</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+        <p class="footer-title">Copyrights @ <span>abdo.dev</span></p>
+        <div class="social-icons">
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+    </footer>
+
+
+</body>
+</html>
